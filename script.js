59cb439d4994cc7a17c4f552ad942bd321683998
looking2bbetter <jtburroughs1998@yahoo.com>
2020-10-31 12:20:07 -0700

    i don't even know... added stuff, somewhat functional?? but definitely not working... until later tonight or tomorrow! I will complete this project tomorrow and before Monday

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+let userAnswer = prompt("Choose rock, paper, or scissors");
+
+let translatedUserAnswer = userAnswer.toUpperCase
+
+console.log(translatedUserAnswer)
+
+let machineAnswer;
+
 let computerPlay = function() {
-    return Math.random() * 10
+    let generatedNumber = randomNumber();
+
+    if (generatedNumber == 1) {
+        machineAnswer = "Rock";
+    } else if (generatedNumber == 2) {
+        machineAnswer = "Paper";
+    } else {
+        machineAnswer = "Scissors - snip snip";
+    }
+}
+
+console.log(machineAnswer);
+
+let win = "You Have Won!!!";
+let lose = "You have lost :(";
+let tie = "You Tied! Try again to Win!";
+
+if (userAnswer == "ROCK" && machineAnswer == "Scissors - snip snip") {
+    console.log(win);
+} else if (userAnswer == "ROCK" && machineAnswer == "Paper") {
+    console.log(lose);
+} else if (userAnswer == "PAPER" && machineAnswer == "Rock") {
+    console.log(win);
+} else if (userAnswer == "PAPER" && machineAnswer == "Scissors - snip snip") {
+    console.log(lose);
+} else if (userAnswer == "SCISSORS" && machineAnswer == "Paper") {
+    console.log(win);
+} else if (userAnswer == "SCISSORS" && machineAnswer == "Rock") {
+    console.log(lose);
+} else {
+    console.log(tie);
 }
 
 let round = function(playerSelection, computerSelection) {
@@ -8,8 +46,10 @@ let round = function(playerSelection, computerSelection) {
     }
 }
 
-let randomMM = function(min, max) {
-    let wholeRando = Math.trunc(Math.random() * 10);
+let randomNumber = function() {
 
-    
+    let generatedNumber = Math.floor(Math.random() * 3) + 1;
+
+    console.log(generatedNumber);
+
 }
