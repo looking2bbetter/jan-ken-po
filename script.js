b815787f6673a8c969a9c586e45b4a0054d1241b
looking2bbetter <jtburroughs1998@yahoo.com>
2020-11-01 16:37:44 -0800

    added returns to functions.... forgot I needed this to get the values created in the functions, out of the functions... YEET

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let userAnswer = prompt("Choose rock, paper, or scissors");
 
-let translatedUserAnswer = userAnswer.toUpperCase
+let translatedUserAnswer = userAnswer.toUpperCase();
 
-console.log(translatedUserAnswer)
+console.log(translatedUserAnswer);
 
 let machineAnswer;
 
 let computerPlay = function() {
-    let generatedNumber = randomNumber();
 
-    if (generatedNumber == 1) {
-        machineAnswer = "Rock";
-    } else if (generatedNumber == 2) {
-        machineAnswer = "Paper";
+    let randomNumber = function() {
+
+        return generatedNumber = Math.floor(Math.random() * 3) + 1;
+    
+    }
+
+    let computerNumber = randomNumber();
+
+    if (computerNumber == 1) {
+        return machineAnswer = "Rock";
+    } else if (computerNumber == 2) {
+        return machineAnswer = "Paper";
     } else {
-        machineAnswer = "Scissors - snip snip";
+        return machineAnswer = "Scissors - snip snip";
     }
 }
+
+computerPlay();
 
 console.log(machineAnswer);
 
@@ -24,32 +33,18 @@ let win = "You Have Won!!!";
 let lose = "You have lost :(";
 let tie = "You Tied! Try again to Win!";
 
-if (userAnswer == "ROCK" && machineAnswer == "Scissors - snip snip") {
+if (translatedUserAnswer == "ROCK" && machineAnswer == "Scissors - snip snip") {
     console.log(win);
-} else if (userAnswer == "ROCK" && machineAnswer == "Paper") {
+} else if (translatedUserAnswer == "ROCK" && machineAnswer == "Paper") {
     console.log(lose);
-} else if (userAnswer == "PAPER" && machineAnswer == "Rock") {
+} else if (translatedUserAnswer == "PAPER" && machineAnswer == "Rock") {
     console.log(win);
-} else if (userAnswer == "PAPER" && machineAnswer == "Scissors - snip snip") {
+} else if (translatedUserAnswer == "PAPER" && machineAnswer == "Scissors - snip snip") {
     console.log(lose);
-} else if (userAnswer == "SCISSORS" && machineAnswer == "Paper") {
+} else if (translatedUserAnswer == "SCISSORS" && machineAnswer == "Paper") {
     console.log(win);
-} else if (userAnswer == "SCISSORS" && machineAnswer == "Rock") {
+} else if (translatedUserAnswer == "SCISSORS" && machineAnswer == "Rock") {
     console.log(lose);
 } else {
     console.log(tie);
-}
-
-let round = function(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('The Computer Wins!!!')
-    }
-}
-
-let randomNumber = function() {
-
-    let generatedNumber = Math.floor(Math.random() * 3) + 1;
-
-    console.log(generatedNumber);
-
 }
